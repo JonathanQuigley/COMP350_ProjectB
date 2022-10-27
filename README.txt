fed6f525a087aff1b2879b834a0508a3c388f4e5
Jonathan Quigley <80792730+JonathanQuigley@users.noreply.github.com>
2022-10-27 12:46:13 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+All steps for the project works, I created the printString, readString, readSector, and handleInterrupt21 functions.
+To verify that printString (step 1) solely works, uncomment line 12 in the main and comment out the rest in main besides the while loop.
+To verify that readString+printString (step 2) works, uncomment lines 15,16, and 17 in the main and comment out the rest in main
+besides the while loop.
+To verify that readSector (step 3) solely works, uncomment line 18 and comment out the rest besides the while loop in main.
+To verify that interrupt21 (step 4 and 5) work, keep the code as is.
---
+++ kernel.c
@@ -0,0 +1,119 @@
+//Name: Jonathan Quigley
+//Worked with: Josh Sproul
+void printString(char*);
+void printChar(char);
+void readString(char*);
+void readSector();
+void handleInterrupt21();
+
+void main(){
+
+/*	interrupt(0x10, 0xe*256+'Q', 0, 0, 0);
+	printString("Hello world");*/
+	char buffer[512];
+	char* line[80];
+/*	printString("Enter a line: ");
+	readString(line);
+	printString(line);
+	readSector(buffer, 30);
+	printString(buffer);
+*/
+	makeInterrupt21();
+	interrupt(0x21, 0, "Enter a line: ", 0, 0);
+	interrupt(0x21, 1, line, 0, 0);
+	interrupt(0x21, 0, line, 0, 0);
+	interrupt(0x21, 2, buffer, 30, 0);
+	interrupt(0x21, 0, buffer, 0, 0);
+	interrupt(0x21, 3, 0, 0, 0);
+	while(1);
+
+}
+
+void printString(char* chars){
+
+	int c = 0;
+	while(chars[c] != 0x0){
+		printChar(chars[c]);
+		c++;
+	}
+
+}
+
+void printChar(char c){
+
+	c= interrupt(0x10, 0xe*256+c, 0, 0, 0);
+
+}
+
+void readString(char* line){
+
+	char c;
+	int n = 0;
+	while(1){
+		c = interrupt(0x16, 0, 0, 0, 0);
+		if(c == 0xd){
+			break;
+		}
+		if(c == 0x8){
+			line[n--];
+			printChar(0x8);
+			printChar(' ');
+			printChar(0x8);
+		}
+		else{
+			line[n++] = c;
+			printChar(c);
+		}
+	}
+	line[n++] = 0xd;
+	line[n++] = 0xa;
+	line[n++] = 0x0;
+	printChar(0xd);
+	printChar(0xa);
+
+}
+
+void readSector(char* buffer, int sector){
+/*
+	int AH = 2;
+	int AL = 1;
+	int CH = 0;
+	int CL = (sector +1);
+	int DH = 0;
+	int DL = 0x80;
+*/
+//optimized code with less integers (AL, AH, CL, CH, etc.)
+	int AX = 2*256 + 1;
+	int BX = buffer;
+	int CX = 0*256 + (sector+1);
+	int DX = 0*256 + (0x80);
+	interrupt(0x13, AX, BX, CX, DX);
+
+}
+
+void handleInterrupt21(int ax, int bx, int cx, int dx){
+
+	//printString("Hello world");
+	if(ax == 0){
+		printString(bx);
+	}
+	else if(ax == 1){
+		readString(bx);
+	}
+	else if(ax == 2){
+		readSector(bx, cx);
+	}
+	else{
+		printString("Out of Bounds exception for interrupt");
+	}
+
+}
+
+
+
+
+
+
+
+
+
